176b9781b09a6a781852f3e0583ff0e07a0d0512
Memo Villalta <guillermo.villalta@taxbit.com>
2023-09-15 02:22:36 -0600

    testing endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useGetFeed } from "./api/endpoints/public-feed/feedGetApi";
 
-export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const queryClient = new QueryClient();
+
+export const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <QueryTest />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </QueryClientProvider>
+  );
+};
+
+const QueryTest = () => {
+  const { data, isLoading, isError } = useGetFeed();
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <div>Error</div>
+      ) : (
+        <div>{JSON.stringify(data)}</div>
+      )}
+    </div>
+  );
+};
+
 export default App;
